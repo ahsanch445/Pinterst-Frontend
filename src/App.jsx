0a63a91784ccf82748cd44d9b597848f1181f25d
Ahsan Ch <ach374392@gmail.com>
2024-03-02 18:11:33 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -46,9 +46,9 @@ if(token){
 
 <Route path="/signup" element={<SignUp />} />
                 <Route path="/login" element={<Login />} />
-       
-         <Route path="/" element={<SignUp />} />
-         <Route path="/" element={< Login />} />
+                <Route path="/" element={< Login />} />
+         {/* <Route path="/" element={<SignUp />} /> */}
+     
         
        
           
