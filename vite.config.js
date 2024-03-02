59bc4aebc502eeb404030b186e6c21940a61cbe5
Ahsan Ch <ach374392@gmail.com>
2024-03-02 22:51:19 +0500

    khjh

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default {
   plugins: [react()],
   build: {
-    outDir: 'build', // Output directory ko customize karne ke liye
+    outDir: 'build', 
   },
 };
