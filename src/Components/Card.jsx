135f079e6c0c8cf2e7c36f6d64fb3e4273b3f583
Ahsan Ch <ach374392@gmail.com>
2024-03-04 23:57:34 +0500

    g

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Card = (data) => {
-
+console.log(data)
   return (
     <div>
    
@@ -12,13 +12,13 @@ const Card = (data) => {
     
         <div class="bg-white p-2 rounded-md shadow-md">
             <img src={data.data.post} alt="Image 2" class="w-full h-auto object-cover mb-1 rounded-xl"/>
-            <p  style={{lineHeight:"1.2 ",opacity:"0.8", fontSize:"1.5vw"}} className='p-1'>{data.data.description} </p>
+            <p  style={{lineHeight:"1.2 ",opacity:"0.8", fontSize:"1.5vw"}} className='p-1'>{data.data.title} </p>
        <div className='flex items-center gap-1 p-2'>
 
         {
           data.data.title&&data.data.description?<img className=' rounded-full w-[3vw]  h-[3vw] object-cover ' src={data.data.user.image} alt="" />:""
         }
-        <h2 style={{fontFamily:"arial", opacity:"0.8"}} className='text-[1.4vw] tracking-tight'>{data.data.title
+        <h2 style={{fontFamily:"arial", opacity:"0.8"}} className='text-[1.4vw] tracking-tight'>{data.user.username
 }</h2>
        </div>
         </div>
