e4602f6c330c498c6cbe530d1a85521a04539a11
Ahsan Ch <ach374392@gmail.com>
2024-03-05 21:07:54 +0500

    add me

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -20,10 +20,10 @@ const Profile = () => {
 <div className=' mt-[2vw] '>
     <div className='flex justify-center'><img style={{ objectFit:"cover",objectPosition:"top"}} className='h-[10vw]  w-[10vw]  rounded-full bg-gray-400' src={userAuth.image} alt="" /></div>
 
-<div className=' w-32'>
-  <h1 style={{fontFamily:"arial",marginTop:"1vw"}} className='text-[3vw] font-bold flex justify-center capitalize'>{userAuth.name}</h1>
+<div className=' w-full'>
+  <h1 style={{fontFamily:"arial",marginTop:"1vw"}} className='text-[3vw] font-bold flex justify-center capitalize '>{userAuth.name}</h1>
   <p className='text-center'>{userAuth.username}</p>
-<p style={{wordWrap:"break-word",lineHeight:"1.3"}} className='text-[1.5vw] text-center'>{userAuth.about}</p>
+<p style={{wordWrap:"break-word",lineHeight:"1.3"}} className='text-[1.5vw] text-center w-32'>{userAuth.about}</p>
 
 <Link to="/ProfileUpdate" style={{fontSize:"arial"}} className=' flex  justify-center bg-[#e1e1e1] w-full p-[1.4vw] rounded-full mt-[1vw] '>Edit Profile</Link>
 </div>
